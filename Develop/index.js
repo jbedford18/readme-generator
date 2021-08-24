0580ec36912509036f90b0cfd0f63ff2cb373aef
Justin B <jbedford94@gmail.com>
2021-08-23 23:20:18 -0400

    initial

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,44 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateReadme = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "Title",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        }
+    ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+promptUser()
+  .then(promptProject)
+  .then(readmeData => {
+    const pageHTML = generatePage(readmeData);
+
+     fs.writeFile('./generateMarkdown.js', Readme, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
 
 // TODO: Create a function to initialize app
 function init() {}
