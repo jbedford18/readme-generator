433abb3b008c347b89fa93d690ce96766bf7909d
Justin B <jbedford94@gmail.com>
2021-08-24 23:02:07 -0400

    added in questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,11 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateReadme = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-function promptUser(){
-    return inquirer.prompt([
-        {
+const questions =[{
             type: "input",
             name: "Title",
             message: "What is the project title?",
@@ -15,33 +13,58 @@ function promptUser(){
         {
             type: "input",
             name: "description",
-            message: "Write a brief description of your project: "
-        }
-    ])
-    .then(projectData => {
-      portfolioData.projects.push(projectData);
-      if (projectData.confirmAddProject) {
-        return promptProject(portfolioData);
-      } else {
-        return portfolioData;
-      }
-    });
-};
+            message: "Describe your project:"
+        },
+        {
+            type: "input",
+            name: "Table of Contents",
+            message: "Create table of contents:"
+        },
+        {
+            type: "input",
+            name: "install",
+            message: "What installation is needed? "
+        },
+        {
+            type: "input",
+            name: "Usage",
+            message: "In what way would you use this project?"
+        },
+        {
+            type: "input",
+            name: "Credits",
+            message: "Who created this project? (github username?) "
+        },
+        {
+            type: "input",
+            name: "License",
+            message: "What licencse is being used for this project?"
+        }]
+
 // TODO: Create a function to write README file
-promptUser()
-  .then(promptProject)
-  .then(readmeData => {
-    const pageHTML = generatePage(readmeData);
+function wrtteToFIle(fileName, data) {
 
-     fs.writeFile('./generateMarkdown.js', Readme, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
-  });
+fs.writeFile(fileName,data, function(err){
+    console.log(fileName)
+    console.log(data)
+    if (err){
+        return console.log(err)
+    }
+        else {
+            console.log("success")
+    
+    }
+})
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data)
+    {
+        writeToFile("README.md", generatorMarkdown(data));}
+    )
+}
 
 // Function call to initialize app
 init();
